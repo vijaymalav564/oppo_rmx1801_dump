67f4b6c7e2844bf73eded506d81eaa05ecb99e35
root <root@fv-az399-494.4urtqdmtpd2erju50p4tzyn2ng.bx.internal.cloudapp.net>
2022-06-23 08:02:51 +0000

    Add extras for derp_RMX1801-userdebug 12 SQ3A.220605.009.B1 1655612389 release-keys
    Signed-off-by: root <root@fv-az399-494.4urtqdmtpd2erju50p4tzyn2ng.bx.internal.cloudapp.net>

---
+++ README.md
@@ -0,0 +1,21 @@
+## derp_RMX1801-userdebug 12 SQ3A.220605.009.B1 1655612389 release-keys
+- Manufacturer: oppo
+- Platform: sdm660
+- Codename: RMX1801
+- Brand: oppo
+- Flavor: derp_RMX1801-userdebug
+- Release Version: 12
+- Id: SQ3A.220605.009.B1
+- Incremental: 1655612389
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: en-US
+- Screen Density: undefined
+- Fingerprint: oppo/derp_RMX1801/RMX1801:12/SQ3A.220605.009.B1/private06190418:userdebug/release-keys
+- OTA version: 
+- Branch: derp_RMX1801-userdebug-12-SQ3A.220605.009.B1-1655612389-release-keys
+- Repo: oppo_rmx1801_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cat system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null >> system/system/system_ext/priv-app/Settings/Settings.apk
+rm -f system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat system/system/product/priv-app/DevicePersonalizationPrebuiltPixel2021/DevicePersonalizationPrebuiltPixel2021.apk.* 2>/dev/null >> system/system/product/priv-app/DevicePersonalizationPrebuiltPixel2021/DevicePersonalizationPrebuiltPixel2021.apk
+rm -f system/system/product/priv-app/DevicePersonalizationPrebuiltPixel2021/DevicePersonalizationPrebuiltPixel2021.apk.* 2>/dev/null
+cat system/system/product/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> system/system/product/priv-app/Velvet/Velvet.apk
+rm -f system/system/product/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat system/system/product/priv-app/PrebuiltBugle/PrebuiltBugle.apk.* 2>/dev/null >> system/system/product/priv-app/PrebuiltBugle/PrebuiltBugle.apk
+rm -f system/system/product/priv-app/PrebuiltBugle/PrebuiltBugle.apk.* 2>/dev/null
+cat system/system/product/priv-app/PrebuiltGmsCoreSc/PrebuiltGmsCoreSc.apk.* 2>/dev/null >> system/system/product/priv-app/PrebuiltGmsCoreSc/PrebuiltGmsCoreSc.apk
+rm -f system/system/product/priv-app/PrebuiltGmsCoreSc/PrebuiltGmsCoreSc.apk.* 2>/dev/null
+cat system/system/product/app/Photos/Photos.apk.* 2>/dev/null >> system/system/product/app/Photos/Photos.apk
+rm -f system/system/product/app/Photos/Photos.apk.* 2>/dev/null
+cat system/system/product/app/YouTube/YouTube.apk.* 2>/dev/null >> system/system/product/app/YouTube/YouTube.apk
+rm -f system/system/product/app/YouTube/YouTube.apk.* 2>/dev/null
+cat system/system/product/app/LatinIMEGooglePrebuilt/LatinIMEGooglePrebuilt.apk.* 2>/dev/null >> system/system/product/app/LatinIMEGooglePrebuilt/LatinIMEGooglePrebuilt.apk
+rm -f system/system/product/app/LatinIMEGooglePrebuilt/LatinIMEGooglePrebuilt.apk.* 2>/dev/null
+cat system/system/product/app/PrebuiltGmail/PrebuiltGmail.apk.* 2>/dev/null >> system/system/product/app/PrebuiltGmail/PrebuiltGmail.apk
+rm -f system/system/product/app/PrebuiltGmail/PrebuiltGmail.apk.* 2>/dev/null
+cat system/system/product/app/Maps/Maps.apk.* 2>/dev/null >> system/system/product/app/Maps/Maps.apk
+rm -f system/system/product/app/Maps/Maps.apk.* 2>/dev/null
